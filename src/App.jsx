be23e666297b57d11a02a0998a3c2dcb91713191
NataschaBGB <nannabjerning@hotmail.com>
2026-01-07 13:49:39 +0100

    add: form, schema and error messages
    set up the form and schema to validate each field and hold error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import RegistrationForm from './assets/components/RegistrationForm'
 import './styles/_layout.sass'
 
 export default function App() {
@@ -5,7 +6,7 @@ export default function App() {
   return (
     <main className="zod">
       <h1>Zod Validation</h1>
-
+      <RegistrationForm />
     </main>
   )
 }
